fc7fb6b00ab192032e1f66ddf99174758564b932
Larissa Nascimento <120027246+larissaNa@users.noreply.github.com>
2025-02-13 16:22:24 -0300

    Update and rename projeto_notas_musicais to projeto_notas_musicais.c
    adicionando display oled

--- projeto_notas_musicais.c
+++ projeto_notas_musicais.c
@@ -10,7 +10,7 @@
 #include "hardware/pwm.h"
 #include "hardware/i2c.h"
 #include "pico/binary_info.h"
-#include "ssd1306.h"
+#include "inc/ssd1306.h"
 
 // Prototipação das funções utilizadas
 void pwm_init_buzzer(uint pin);
@@ -221,6 +221,49 @@ int main() {
     gpio_set_dir(LED_RED, GPIO_OUT);
     
     init_microphone();
+
+     // Inicialização do i2c
+     i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+     gpio_pull_up(I2C_SDA);
+     gpio_pull_up(I2C_SCL);
+    // Processo de inicialização completo do OLED SSD1306
+    ssd1306_init();
+
+    // Preparar área de renderização para o display (ssd1306_width pixels por ssd1306_n_pages páginas)
+    struct render_area frame_area = {
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1
+    };
+
+    calculate_render_area_buffer_length(&frame_area);
+
+    // zera o display inteiro
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+
+restart:
+
+// Parte do código para exibir a mensagem no display (opcional: mudar ssd1306_height para 32 em ssd1306_i2c.h)
+// /**
+    char *text[] = {
+        "Ola! para",
+        "comecar clique no",
+        "  botao A", 
+};
+
+    int y = 0;
+    for (uint i = 0; i < count_of(text); i++)
+    {
+        ssd1306_draw_string(ssd, 5, y, text[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+
 
     while (1) {
         bool notePlayed = false;
